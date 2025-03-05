3f6b7478c7ce60c4626fc2fbebd440e30b3231b8
Miguel-ectil <ectilmiguel@gmail.com>
2025-03-05 11:54:45 -0300

    [UPD] Melhorando o card de not foud

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,12 +1,14 @@
 export default function NotFound() {
     return (
-      <div className="flex min-h-screen flex-col items-center justify-center bg-gray-100 text-center">
-        <h1 className="text-5xl font-bold text-red-600">404</h1>
-        <p className="text-3xl mt-4 font-bold">Link/Página não encontrada</p>
-        <a href="/" className="mt-6 text-blue-500 hover:underline">
-          Voltar para a Home
-        </a>
+      <div className="flex min-h-screen flex-col items-center justify-center bg-gray-200 text-center px-4">
+      <div className="bg-gray-100 border border-gray-100 rounded-2xl shadow-lg p-8 md:p-12 max-w-lg w-full sm:w-3/4 lg:w-1/2">
+        <h1 className="text-6xl md:text-7xl font-bold text-red-600">404</h1>
+        <p className="text-2xl md:text-3xl mt-6 font-bold">Link/Página não encontrada</p>
+        <p className="mt-6  hover:underline text-lg md:text-sm block">
+          O link que você está tentando acessar não existe, foi removido ou é uma URL inválida. Saiba mais em brev.ly.
+        </p>
       </div>
+    </div>    
     );
   }
   
