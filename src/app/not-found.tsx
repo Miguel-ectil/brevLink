4b326f0340161c707fec9baa69d4cc02658f3fea
Miguel-ectil <ectilmiguel@gmail.com>
2025-03-05 13:04:14 -0300

    [UPD] Melhorando

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function NotFound() {
   return (
-      <div className="flex min-h-screen items-center justify-center bg-gray-200 px-4">
+    <div className="flex min-h-screen items-center justify-center bg-gray-200 px-4">
       <div className="bg-gray-100 border border-gray-300 rounded-2xl shadow-lg p-8 md:p-12 max-w-lg w-full sm:w-3/4 lg:w-1/2 flex flex-col items-center text-center">
         <Image src="/images/404.svg" alt="Ícone not found" width={180} height={240} className="mx-auto" />
         <p className="text-1xl md:text-2xl mt-6 font-bold">Link/Página não encontrada</p>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,20 @@ import ThemeToggle from "@/components/ThemeToggle";
 
 export default function Home() {
   return (
-    <div className="min-h-screen">
+    <div className="flex min-h-screen items-center justify-center space-x-6 px-4">
+      <div className="bg-gray-100 border border-gray-300 rounded-2xl shadow-lg p-8 md:p-12 max-w-lg w-full sm:w-3/4 lg:w-1/2 flex flex-col items-center text-center">
+        <p className="text-1xl md:text-2xl mt-6 font-bold">Novo Link</p>
+        <p className="mt-6 text-lg md:text-xl text-gray-600 max-w-md">
+          O link que você está tentando acessar não existe, foi removido ou é uma URL inválida. Saiba mais em <a href="https://brev.ly" className="text-blue-500 hover:underline">brev.ly</a>.
+        </p>
+      </div>
+      {/* Card da Direita (Menor) */}
+      <div className="bg-gray-100 border border-gray-300 rounded-2xl shadow-lg p-8 md:p-12 w-full sm:w-3/4 lg:w-1/3 flex flex-col items-center text-center">
+        <p className="text-2xl md:text-3xl mt-6 font-bold">Meus Links</p>
+        <p className="mt-6 text-lg md:text-xl text-gray-600 max-w-md">
+          O link que você está tentando acessar não existe, foi removido ou é uma URL inválida. Saiba mais em <a href="https://brev.ly" className="text-blue-500 hover:underline">brev.ly</a>.
+        </p>
+      </div>
       <ThemeToggle />
     </div>
   );
