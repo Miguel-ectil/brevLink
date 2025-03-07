93c8d26cff28af899f13825e621cfd94d8cf9d9e
Miguel-ectil <ectilmiguel@gmail.com>
2025-03-07 11:43:29 -0300

    [ADD] Recuperando valor da api do back-end na tela

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,25 @@
 "use client"
 import { useEffect, useState } from "react";
 import ThemeToggle from "@/components/ThemeToggle";
-import axios from "axios";
+import { getLinks } from "@/services/apiService"; // Importa a função que busca os links
 import Image from "next/image";
 
+type Link = {
+  id: number;
+  short_url: string;
+  original_url: string;
+  clicks: number;
+};
+
 export default function Home() {
-  const [linksData, setLinksData] = useState([]);
+  const [linksData, setLinksData] = useState<Link[]>([]);
 
   useEffect(() => {
     const fetchLinks = async () => {
       try {
-        const response = await axios.get("https://api.exemplo.com/links"); // Substitua pela URL da sua API
-        setLinksData(response.data);
+        const data = await getLinks(); // Agora usa a função getLinks para buscar os dados
+        if (data) setLinksData(data);
+        console.log('Olha os dados', data)
       } catch (error) {
         console.error("Erro ao buscar os dados da API", error);
       }
@@ -19,9 +27,11 @@ export default function Home() {
 
     fetchLinks();
   }, []);
+
   return (
     <div className="min-h-screen flex flex-col items-center justify-center">
       {/* Título */}
+      {/* <div>sdzfsfgsdgsd;f {linksData}</div> */}
       <div className="font-bold text-2xl md:text-3xl my-6 text-left pl-4">
         <Image src="icons/Logo.svg" width={120} height={120} alt="CV" />
       </div>
@@ -82,52 +92,49 @@ export default function Home() {
           {/* Linha separadora */}
           <div className="w-full border-t border-gray-300 my-4"></div>
 
-          <div className="">
-            <table>
-              {/* <thead>
-            <tr>
-              <th className="p-4 text-left">Links</th>
-              <th className="p-4 text-left">Acessos</th>
-              <th className="p-4 text-left">Ações</th>
-            </tr>
-          </thead> */}
+          <div className="overflow-x-auto w-full"> {/* Torna a tabela rolável em telas pequenas */}
+            <table className="min-w-full table-auto border-collapse">
               <tbody>
                 {linksData.length === 0 ? (
-                  <tr className="w-full ">
-                    <td colSpan={3} className="flex flex-col items-center justify-center text-gray-500">
+                  <tr>
+                    <td colSpan={3} className="flex flex-col items-center justify-center text-gray-500 py-8">
                       <div className="mb-4">
                         <Image src="icons/Link.svg" width={42} height={20} alt="CV" className="text-gray-400" />
                       </div>
-                      <p>
-                        AINDA NÃO EXISTE LINKS CADASTRADOS.
-                      </p>
+                      <p>AINDA NÃO EXISTE LINKS CADASTRADOS.</p>
                     </td>
                   </tr>
                 ) : (
                   linksData.map((link) => (
-                    <tr key={link.id}>
-                      <td className="p-4 text-gray-600">{link.url}</td>
-                      <td className="p-4 text-gray-600">{link.acessos}</td>
-                      <td className="flex p-4">
+                    <tr key={link.id} className="border-b border-gray-300 hover:bg-gray-50">
+                      <td className="text-gray-600 text-left">
+                        <div className="text-xl font-bold text-blue-600 hover:text-blue-700">{link.short_url}</div>
+                        <p className="text-sm text-gray-500">{link.original_url}</p>
+                      </td>
+                      <td className="p-4 text-gray-600 text-left">{link.clicks} acessos</td>
+                      <td className="flex py-4 space-x-2 justify-start">
                         <button
-                          className="mr-2 p-2 text-white bg-[#CDCFD5] rounded-md hover:bg-[#74798B]"
-                          onClick={() => alert(`Editando ${link.url}`)}
+                          className="p-1.5 text-white bg-[#CDCFD5] rounded-md hover:bg-[#74798B]"
+                          onClick={() => alert(`Editando ${link.id}`)}
                         >
-                          <Image src="icons/Copy.svg" width={22} height={20} alt="CV" className="text-gray-400" />
+                          <Image src="icons/Copy.svg" width={24} height={24} alt="CV" className="text-gray-400 " />
                         </button>
                         <button
-                          className="p-2 text-white bg-[#CDCFD5] rounded-md hover:bg-[#74798B]"
-                          onClick={() => alert(`Excluindo ${link.url}`)}
+                          className="p-1.5 text-white bg-[#CDCFD5] rounded-md hover:bg-[#74798B]"
+                          onClick={() => alert(`Excluindo ${link.id}`)}
                         >
-                          <Image src="icons/Trash.svg" width={22} height={20} alt="CV" className="text-gray-400" />
+                          <Image src="icons/Trash.svg" width={24} height={24} alt="CV" className="text-gray-400 sm:w-6 sm:h-6" />
                         </button>
                       </td>
                     </tr>
                   ))
                 )}
               </tbody>
+
             </table>
           </div>
+
+
         </div>
       </div>
       <ThemeToggle />
--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -8,23 +8,23 @@ const api = axios.create({
 });
 
 // Interceptor para adicionar o token na requisição
-api.interceptors.request.use(
-  (config) => {
-    const token = getToken();
-    if (token) {
-      config.headers["Authorization"] = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
+// api.interceptors.request.use(
+//   (config) => {
+//     const token = getToken();
+//     if (token) {
+//       config.headers["Authorization"] = `Bearer ${token}`;
+//     }
+//     return config;
+//   },
+//   (error) => {
+//     return Promise.reject(error);
+//   }
+// );
 
 // Exemplo de uma função para fazer uma requisição
 export const getLinks = async () => {
   try {
-    const response = await api.get("/links");
+    const response = await api.get("/links?id=c292c8d1-1ab1-4bd4-af31-278bab477462");
     return response.data;
   } catch (error) {
     throw new Error("Erro ao buscar links");
